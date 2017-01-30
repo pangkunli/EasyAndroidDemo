ef8a2503472711fe9d418cbe77818660f1190540
pangkunli <358352764@qq.com>
2017-01-30 17:11:27 +0800

    A : 工程

---
+++ hanjia/app/src/main/java/com/example/cui/hanjia/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cui.hanjia;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
